f6fcd97091b3bd4559698399bc0239abfd95b6a5
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-24 23:15:33 -0400

    Create MySnippet.py

---
+++ Python-Lists/MySnippet.py
@@ -0,0 +1,8 @@
+cources = ['History', 'Math', 'Physics', 'CompSci']
+print(cources)      #['History', 'Math', 'Physics', 'CompSci']
+print(len(cources)) #4
+print(cources[3])   #CompSci
+print(cources[-1])  #CompSci
+print(cources[0:2]) #['History', 'Math']
+print(cources[:2]) #['History', 'Math']
+print(cources[2:]) #['Physics', 'CompSci']
