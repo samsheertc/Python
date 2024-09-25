22438092ef829c54823c4e1598dd67585c93f93a
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-25 15:05:47 -0400

    Update intro.py

--- Python-Imports/intro.py
+++ Python-Imports/intro.py
@@ -1,2 +1,34 @@
+#courses = ['History', 'Math', 'Physics', 'CompSci']
 
-courses = ['History', 'Math', 'Physics', 'CompSci']
+import my_module
+find_list = ['Math', 'Phy', 'Eng', 'Bio']
+find_str = 'Bio'
+pos = my_module.find_index(find_str,find_list)
+print(pos)
+
+import my_module as tm
+find_list = ['Math', 'Phy', 'Eng', 'Bio']
+find_str = 'Bio'
+pos = tm.find_index(find_str,find_list)
+print(pos)
+
+from my_module import find_index, test
+find_list = ['Math', 'Phy', 'Eng', 'Bio']
+find_str = 'Bio'
+pos = find_index(find_str,find_list)
+print(pos)
+print(test)
+
+from my_module import find_index as find, test
+find_list = ['Math', 'Phy', 'Eng', 'Bio']
+find_str = 'Bio'
+pos = find(find_str,find_list)
+print(pos)
+print(test)
+
+from my_module import *
+find_list = ['Math', 'Phy', 'Eng', 'Bio']
+find_str = 'Bio'
+pos = find_index(find_str,find_list)
+print(pos)
+print(test)
