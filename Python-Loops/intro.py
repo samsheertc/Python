68aa2b2f866a4fd193ab8dda31d5d80459963fa7
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-25 11:25:42 -0400

    Update intro.py

--- Python-Loops/intro.py
+++ Python-Loops/intro.py
@@ -6,3 +6,42 @@ while True:
     #     break
     print(x)
     x += 1
+
+
+#break in Loop
+nums = [1, 2, 3, 4, 5]
+for num in nums:
+    if num==3:
+        print("found")
+        break
+    print(num)
+
+#continue in Loop
+for num in nums:
+    if num==3:
+        print("found")
+        continue
+    print(num)
+
+#Nested Loop
+for num in nums:
+    for letter in 'abc':
+        print(num,letter)
+
+#Range Loop
+for i in range(1,10,2):
+    print(i)
+    
+#While Loop
+i=0
+while i < 10:
+    if i ==5:
+        break
+    print(i)
+    i=i+1
+
+while True:
+    if i == 5:
+        break
+    print(i)
+    i = i + 1
