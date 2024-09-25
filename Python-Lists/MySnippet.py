0f457d88ca85510dfd61c9a4424d23301d0af42e
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-24 23:23:56 -0400

    Update MySnippet.py

--- Python-Lists/MySnippet.py
+++ Python-Lists/MySnippet.py
@@ -4,5 +4,28 @@ print(len(cources)) #4
 print(cources[3])   #CompSci
 print(cources[-1])  #CompSci
 print(cources[0:2]) #['History', 'Math']
-print(cources[:2]) #['History', 'Math']
-print(cources[2:]) #['Physics', 'CompSci']
+print(cources[:2])  #['History', 'Math']
+print(cources[2:])  #['Physics', 'CompSci']
+
+#Append Method
+cources = ['History', 'Math', 'Physics', 'CompSci']
+cources.append('Art')
+print(cources)   #['History', 'Math', 'Physics', 'CompSci', 'Art']
+
+#insert
+cources = ['History', 'Math', 'Physics', 'CompSci']
+cources.insert(0,'Art')
+print(cources) #['Art', 'History', 'Math', 'Physics', 'CompSci']
+
+cources = ['History', 'Math', 'Physics', 'CompSci']
+cources2 = ['Art', 'Education']
+cources.insert(0,cources2)
+print(cources)    #[['Art', 'Education'], 'History', 'Math', 'Physics', 'CompSci']
+print(cources[0]) #['Art', 'Education']
+
+
+#extend
+cources = ['History', 'Math', 'Physics', 'CompSci']
+cources2 = ['Art', 'Education']
+cources.extend(cources2)
+print(cources)  #['History', 'Math', 'Physics', 'CompSci', 'Art', 'Education']
