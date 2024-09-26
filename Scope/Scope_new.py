2396f28b3ec1585b6fa974bd8331a3b7126533b3
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-25 21:44:39 -0400

    Create Scope_new.py

---
+++ Scope/Scope_new.py
@@ -0,0 +1,37 @@
+'''
+LEGB
+Local, Enclosing, Global, Built-in
+'''
+
+x = 'global x'
+def test():
+    global x
+    x = 'local x'
+    y = 'local y'
+    print(y) #local y
+    print(x) #local x
+test()
+print(x)
+#print(y)
+
+
+import builtins
+import sys-
+import datetime
+print(dir(builtins))
+print(dir(sys))
+print(dir(datetime))
+
+
+#Enclosing Example
+x = 'global x'
+def outer():
+    #x = 'outer x'
+    def inner():
+        #nonlocal x
+        #x = 'inner x'
+        print(x)
+    inner()
+    print(x)
+outer()
+print(x)
