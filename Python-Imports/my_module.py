5ff8193b3632b1643a0c0817cc23646e19d9c30a
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-26 11:20:22 -0400

    Update my_module.py

--- Python-Imports/my_module.py
+++ Python-Imports/my_module.py
@@ -1,13 +1,10 @@
-
 print('Imported my_module...')
-
 test = 'Test String'
 
-
-def find_index(to_search, target):
+def find_index(target ,to_search):
     '''Find the index of a value in a sequence'''
-    for i, value in enumerate(to_search):
-        if value == target:
+    for i, value in enumerate(target):
+        if value == to_search:
             return i
 
     return -1
