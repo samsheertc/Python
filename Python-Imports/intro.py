39df25ce29c25cf207001f71c68adae474edc182
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-26 11:18:39 -0400

    Update intro.py

--- Python-Imports/intro.py
+++ Python-Imports/intro.py
@@ -3,32 +3,32 @@
 import my_module
 find_list = ['Math', 'Phy', 'Eng', 'Bio']
 find_str = 'Bio'
-pos = my_module.find_index(find_str,find_list)
+pos = my_module.find_index(find_list, find_str)
 print(pos)
 
 import my_module as tm
 find_list = ['Math', 'Phy', 'Eng', 'Bio']
 find_str = 'Bio'
-pos = tm.find_index(find_str,find_list)
+pos = tm.find_index(find_list, find_str)
 print(pos)
 
 from my_module import find_index, test
 find_list = ['Math', 'Phy', 'Eng', 'Bio']
 find_str = 'Bio'
-pos = find_index(find_str,find_list)
+pos = find_index(find_list, find_str)
 print(pos)
 print(test)
 
 from my_module import find_index as find, test
 find_list = ['Math', 'Phy', 'Eng', 'Bio']
 find_str = 'Bio'
-pos = find(find_str,find_list)
+pos = find(find_list, find_str)
 print(pos)
 print(test)
 
 from my_module import *
 find_list = ['Math', 'Phy', 'Eng', 'Bio']
 find_str = 'Bio'
-pos = find_index(find_str,find_list)
+pos = find_index(find_list, find_str)
 print(pos)
 print(test)
