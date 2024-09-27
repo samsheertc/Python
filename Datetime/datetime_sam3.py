02af7c239f6deee99eb5bcce82571cc2e795c7ce
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-26 23:29:50 -0400

    Update datetime_sam3.py

--- Datetime/datetime_sam3.py
+++ Datetime/datetime_sam3.py
@@ -4,6 +4,7 @@ import pytz
 dt_utcnow = datetime.datetime.now(tz=pytz.UTC)
 print(dt_utcnow)
 
+#Convert From Mountain time to Eastern
 dt_mtn = datetime.datetime.now()
 #dt_east = dt_mtn.astimezone(pytz.timezone('US/Eastern')) #Error
 mtn_tz = pytz.timezone('US/Mountain')
@@ -12,13 +13,27 @@ print(dt_mtn)
 dt_east = dt_mtn.astimezone(pytz.timezone('US/Eastern'))
 print(dt_east)
 
+dt_Easten = datetime.datetime.now(tz=pytz.timezone('US/Eastern'))          #[Current time in Eastern Area]
+dt_Central = datetime.datetime.now(tz=pytz.timezone('US/Central'))         #[Current time in Central Area]
+dt_Mountain = datetime.datetime.now(tz=pytz.timezone('US/Mountain'))       #[Current time in Mountain Area]
+dt_Pacific = datetime.datetime.now(tz=pytz.timezone('US/Pacific'))         #[Current time in Pacific Area]
 
-dt_mtn = datetime.datetime.now(tz=pytz.timezone('US/Mountain'))
-print(dt_mtn.isoformat())
-print(dt_mtn.strftime('%B %d, %Y'))
+print(dt_Easten)               #2024-09-26 23:26:10.314716-04:00
+print(dt_Central)              #2024-09-26 22:26:10.315714-05:00
+print(dt_Mountain)             #2024-09-26 21:26:10.315714-06:00
+print(dt_Pacific)              #2024-09-26 20:26:10.316711-07:00
+print(dt_Easten.isoformat())   #2024-09-26T23:26:10.314716-04:00
+print(dt_Central.isoformat())  #2024-09-26T22:26:10.315714-05:00
+print(dt_Mountain.isoformat()) #2024-09-26T21:26:10.315714-06:00
+print(dt_Pacific.isoformat())  #2024-09-26T20:26:10.316711-07:00
+
+#Convert using strftime
+print(dt_mtn.strftime('%B %d, %Y'))   #September 26, 2024
+
+#Convert using strptime
 dt_str='September 26, 2024'
 dt = datetime.datetime.strptime(dt_str,'%B %d, %Y')
-print(dt)
+print(dt)                             #2024-09-26 00:00:00
 
 # strftime - Datetime to String
 # strptime - String to Datetime
