4ac34a7de67c397ec871335fcf412a5f07f61a16
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-26 22:59:22 -0400

    Update datetime_sam2.py

--- Datetime/datetime_sam2.py
+++ Datetime/datetime_sam2.py
@@ -10,42 +10,27 @@ print(dt_utcnow) #2024-09-27 00:09:20.121120
 import datetime
 import pytz
 
-#Add timezone to a Date value
-dtc = datetime.datetime(2016,7,26,12,30,45,tzinfo=pytz.UTC)
-print(dtc)
-
-#Current UTC Time
-dt_now = datetime.datetime.now(tz=pytz.UTC)
-print(dt_now) #2024-09-27 00:20:57.427277+00:00
-
-
-#Current UTC Time [Alternate method]
-dt_utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
+dtc = datetime.datetime(2016,7,26,12,30,45,tzinfo=pytz.UTC)      #Convert a Datetime value to TimeZone
+dt_now = datetime.datetime.now(tz=pytz.UTC)                      #Current UTC Time
+dt_utcnow = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)  #Current UTC Time ..Alternate method
+print(dtc)       #2016-07-26 12:30:45+00:00 
+print(dt_now)    #2024-09-27 00:20:57.427277+00:00
 print(dt_utcnow) #2024-09-27 00:20:57.427277+00:00
 
 
-#Conversion from One timezone to Other
-dt_utcnow = datetime.datetime.now(tz=pytz.UTC)
-print(dt_utcnow)
-#2024-09-27 00:25:36.186811+00:00 [Pull Current UTC Time]
+#Conversion from One TIMEZONE to Other
+dt_utcnow = datetime.datetime.now(tz=pytz.UTC)                  #[Pull Current UTC Time]
+dt_india = dt_utcnow.astimezone(pytz.timezone('Asia/Kolkata'))  #[Convert Current UTC Time to Indian Time]
+dt_eastern = dt_utcnow.astimezone(pytz.timezone('US/Eastern'))  #[Convert Current UTC Time to Eastern Time]
+dt_central = dt_utcnow.astimezone(pytz.timezone('US/Central'))  #[Convert Current UTC Time to Central Time]
+dt_moutain = dt_utcnow.astimezone(pytz.timezone('US/Mountain')) #[Convert Current UTC Time to Mountain Time]
+dt_pacific = dt_utcnow.astimezone(pytz.timezone('US/Pacific'))  #[Convert Current UTC Time to Pacific Time]
 
-dt_east = dt_utcnow.astimezone(pytz.timezone('US/Eastern'))
-print(dt_east)
-#2024-09-26 20:25:36.186811-04:00 [Convert Current UTC Time to Eastern Time]
+print(dt_india)     #2024-09-27 05:55:36.186811+05:30
+print(dt_utcnow)    #2024-09-27 00:25:36.186811+00:00
+print(dt_eastern)   #2024-09-26 20:25:36.186811-04:00
+print(dt_central)   #2024-09-26 19:25:36.186811-05:00
+print(dt_moutain)   #2024-09-26 18:25:36.186811-06:00
+print(dt_pacific)   #2024-09-26 17:25:36.186811-07:00
 
-dt_ctrl = dt_utcnow.astimezone(pytz.timezone('US/Central'))
-print(dt_ctrl)
-#2024-09-26 19:25:36.186811-05:00 [Convert Current UTC Time to Central Time]
-
-dt_mtn = dt_utcnow.astimezone(pytz.timezone('US/Mountain'))
-print(dt_mtn)
-#2024-09-26 18:25:36.186811-06:00 [Convert Current UTC Time to Mountain Time]
-
-dt_pac = dt_utcnow.astimezone(pytz.timezone('US/Pacific'))
-print(dt_pac)
-#2024-09-26 17:25:36.186811-07:00 [Convert Current UTC Time to Pacific Time]
-
-dt_ind = dt_utcnow.astimezone(pytz.timezone('Asia/Kolkata'))
-print(dt_ind)
-#2024-09-27 05:55:36.186811+05:30 [Convert Current UTC Time to Indian Time]
 #----------------------------------------------------------------------------------------------------------------
