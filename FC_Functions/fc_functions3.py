ee68df76a6a65f9444f237fb6cc5e7bfe05359ca
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-30 23:29:06 -0400

    Update fc_functions3.py

--- FC_Functions/fc_functions3.py
+++ FC_Functions/fc_functions3.py
@@ -1,7 +1,9 @@
+# Here the logger function returns function "log_message "
+
 def logger(msg):
     def log_message():
         print('Log:',msg)
     return log_message
 
 log_hi = logger('Hi!')
-log_hi()
+log_hi()    #Log: Hi!
