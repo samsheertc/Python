dc07458cb0f77948cd3fb3a7efedd11a17fae571
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-30 23:03:58 -0400

    Update fc_functions1.py

--- FC_Functions/fc_functions1.py
+++ FC_Functions/fc_functions1.py
@@ -2,6 +2,6 @@ def square(x):
     return x * x
 
 f = square
-print(f)
-print(square)
-print(f(5))
+print(f)      #<function square at 0x000001AACB7F7010>
+print(square) #<function square at 0x000001AACB7F7010>
+print(f(5))   #25
