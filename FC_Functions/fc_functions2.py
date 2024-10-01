d781aca1444fd678087fb83220cea41e1a7a9c05
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-01 12:14:17 -0400

    Update fc_functions2.py

--- FC_Functions/fc_functions2.py
+++ FC_Functions/fc_functions2.py
@@ -1,5 +1,4 @@
 '''
-A Function accepts other functions as arugments or return functions as their result is called as Higher - Order Function
 The below example is similar to a MAP function and it passes an arugment as a function.
 Here the function takes a function and an array as its arguments and run each value of that array through 
 the provided function and returns a new array of those results
