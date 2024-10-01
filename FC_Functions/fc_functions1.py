184593899811e411488e571dd954b309222ff4b3
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-09-30 23:09:35 -0400

    Update fc_functions1.py

--- FC_Functions/fc_functions1.py
+++ FC_Functions/fc_functions1.py
@@ -5,3 +5,5 @@ f = square
 print(f)      #<function square at 0x000001AACB7F7010>
 print(square) #<function square at 0x000001AACB7F7010>
 print(f(5))   #25
+
+#Here the variable hold the name of the function and can call the function using the variable name
