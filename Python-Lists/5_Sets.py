d233cd5635ede5787154afd843f5d0dde3334e5f
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-02 17:08:42 -0400

    Update 5_Sets.py

--- Python-Lists/5_Sets.py
+++ Python-Lists/5_Sets.py
@@ -1,8 +1,22 @@
 
-# Sets
+#Sets
 cs_courses = {'History', 'Math', 'Physics', 'CompSci'}
 print(cs_courses)
 
+#Order in each run
+#{'Physics', 'Math', 'History', 'CompSci'}
+#{'Math', 'Physics', 'History', 'CompSci'}
+#{'CompSci', 'Physics', 'Math', 'History'}
+#{'Physics', 'Math', 'CompSci', 'History'}
+#{'Physics', 'History', 'Math', 'CompSci'}
+
+#Dups are removed
+cs_courses = {'History', 'Math', 'Physics', 'CompSci', 'Math'}
+print(cs_courses) 
+#{'CompSci', 'History', 'Physics', 'Math'}
+
+
+#Membership Test
 cs_courses = {'History', 'Math', 'Physics', 'CompSci', 'CompSci'}
 print('Math' in cs_courses) #True
 
@@ -13,17 +27,3 @@ art_courses = {'History', 'Math', 'Art', 'Design'}
 print(cs_courses.intersection(art_courses)) #{'Math', 'History'}
 print(cs_courses.difference(art_courses))   #{'CompSci', 'Physics'}
 print(cs_courses.union(art_courses))        #{'History', 'Art', 'Design', 'Math', 'Physics', 'CompSci'}
-
-
-
-# Empty Lists
-empty_list = []
-empty_list = list()
-
-# Empty Tuples
-empty_tuple = ()
-empty_tuple = tuple()
-
-# Empty Sets
-empty_set = {}     # This isn't right! It's a dict
-empty_set = set()
