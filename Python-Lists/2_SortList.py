43a505cf33714519451346c418d368ca6fac7a53
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-02 16:42:01 -0400

    Update and rename SortList.py to 2_SortList.py

--- Python-Lists/2_SortList.py
+++ Python-Lists/2_SortList.py
@@ -1,31 +1,48 @@
-cources = ['History', 'Math', 'Physics', 'CompSci']
-cources.remove('Math')
-print(cources)   #['History', 'Physics', 'CompSci']
-
-popped = cources.pop()
-print(popped)  #CompSci
-print(cources) #['History', 'Math', 'Physics']
-
-cources = ['History', 'Math', 'Physics', 'CompSci']
-cources.reverse()
-print(cources) #['CompSci', 'Physics', 'Math', 'History']
-
+#eg-1
+#sort method
 cources = ['History', 'Math', 'Physics', 'CompSci']
 nums = [1, 5, 2, 4, 3]
+
 cources.sort()
 nums.sort()
+
 print(cources) #['CompSci', 'History', 'Math', 'Physics']
 print(nums)    #[1, 2, 3, 4, 5]
 
+
+
+
+#eg-2
 cources = ['History', 'Math', 'Physics', 'CompSci']
 nums = [1, 5, 2, 4, 3]
+
 cources.sort(reverse=True)
 nums.sort(reverse=True)
+
 print(cources) #['Physics', 'Math', 'History', 'CompSci']
 print(nums)    #[5, 4, 3, 2, 1]
 
+
+
+
+#eg-3
+#sorted function
 cources = ['History', 'Math', 'Physics', 'CompSci']
+
 sorted_cources = sorted(cources)
+
 print(cources)         #['History', 'Math', 'Physics', 'CompSci']
 print(sorted_cources)  #['CompSci', 'History', 'Math', 'Physics']
 
+
+
+
+
+#eg-4
+#sorted function
+cources = ['History', 'Math', 'Physics', 'CompSci']
+
+sorted_cources = sorted(cources, reverse=True)
+
+print(cources)         #['History', 'Math', 'Physics', 'CompSci']
+print(sorted_cources)  #['Physics', 'Math', 'History', 'CompSci']
