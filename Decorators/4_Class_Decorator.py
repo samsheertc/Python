4aac04e191f980540ae13d37a9c3a97f9d38e5ea
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-02 10:41:29 -0400

    Update 4_Class_Decorator.py

--- Decorators/4_Class_Decorator.py
+++ Decorators/4_Class_Decorator.py
@@ -2,10 +2,10 @@
 #13:20 - 16:30
 
 class decorator_class(object):
-	
+
     def __init__(self, original_function):
         self.original_function = original_function
-		
+
     def __call__(self, *args, **kwargs):
         print('Call method executed this before {}'.format(self.original_function.__name__))
         return self.original_function(*args, **kwargs)
