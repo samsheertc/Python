7502b97cacf019916b531c8a06ec41709bb20afe
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-02 21:58:51 -0400

    Update 2_Generators.py

--- Generators/2_Generators.py
+++ Generators/2_Generators.py
@@ -5,7 +5,7 @@ def square_numbers(nums):
 
 my_nums = square_numbers([1, 2, 3, 4, 5])
 
-print(my_nums)  #<generator object square_numbers at 0x000002E2BFED2650>
+print(my_nums)       #<generator object square_numbers at 0x000002E2BFED2650>
 print(next(my_nums)) #1
 print(next(my_nums)) #4
 print(next(my_nums)) #9
@@ -13,20 +13,5 @@ print(next(my_nums)) #16
 print(next(my_nums)) #25
 
 my_nums = square_numbers([1, 2, 3, 4, 5])
-for num in my_nums:
-    print(num)
-	
-	
-#List Comprehension
-my_nums = [x*x for x in [1, 2, 3, 4, 5]]
-print(my_nums)  #[1, 4, 9, 16, 25]
-
-#Generator
-my_nums =(x*x for x in [1, 2, 3, 4, 5])
-
-print(my_nums)       #<generator object <genexpr> at 0x0000023858A02570>
-print(list(my_nums)) #[1, 4, 9, 16, 25]
-
-
 for num in my_nums:
     print(num)
