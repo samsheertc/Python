65fc6743e5bdb2cdc51c6c4fce584b3358af6fca
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-05 22:52:43 -0400

    Update named_tuples.py

--- NamedTuples/named_tuples.py
+++ NamedTuples/named_tuples.py
@@ -5,4 +5,6 @@ Color = namedtuple('Color', ['red', 'green', 'blue'])
 color = Color(55,155,255)
 white = Color(255,255,255)
 
-print color.blue
+print (color.red)
+print (color.green)
+print (color.blue)
