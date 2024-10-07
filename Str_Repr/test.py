f2d70790e8788999308f157bc23cad89692d958f
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-07 16:31:38 -0400

    Update test.py

--- Str_Repr/test.py
+++ Str_Repr/test.py
@@ -1,3 +1,17 @@
+a = [1,2,3,4]
+b = 'sample string'
+
+print(str(a))   #[1, 2, 3, 4]
+
+print(repr(a))  #[1, 2, 3, 4]
+
+print(str(b))  #sample string
+
+print(repr(b)) #'sample string'
+
+
+
+
 import datetime
 import pytz
 
@@ -5,12 +19,15 @@ a = datetime.datetime.utcnow().replace(tzinfo=pytz.UTC)
 
 b = str(a)
 
-print 'str(a): {}'.format(str(a))
-print 'str(b): {}'.format(str(b))
+print('str(a): {}'.format(str(a)))    #str(a): 2024-10-07 16:29:58.341528+00:00
 
-print
+print('str(b): {}'.format(str(b)))    #str(b): 2024-10-07 16:29:58.341528+00:00
 
-print 'repr(a): {}'.format(repr(a))
-print 'repr(b): {}'.format(repr(b))
+print()
 
-print
+print('repr(a): {}'.format(repr(a)))  #repr(a): datetime.datetime(2024, 10, 7, 16, 29, 58, 341528, tzinfo=<UTC>)
+
+print('repr(b): {}'.format(repr(b)))  #repr(b): '2024-10-07 16:29:58.341528+00:00'
+
+print()
+
