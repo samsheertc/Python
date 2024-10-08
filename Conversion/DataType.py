1172b7ed5162e575603f330ed46b97d83b0a3cf2
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-08 13:10:27 -0400

    Update DataType.py

--- Conversion/DataType.py
+++ Conversion/DataType.py
@@ -11,3 +11,27 @@ print(f'Tuple to set  \n{my_tup1} \n{set(my_tup1)}\n')
 print(f'List  to set  \n{my_list1}\n{set(my_list1)}\n')
 print(f'Tuple to list \n{my_tup1} \n{list(my_tup1)}\n')
 print(f'List to tuple \n{my_list1}\n{tuple(my_list1)}\n')
+
+
+'''
+Tuple to dictionary
+('key1', 'value1')
+('key2', 'value2')
+{'key1': 'value1', 'key2': 'value2'}
+
+Tuple to set  
+('key1', 'value1') 
+{'value1', 'key1'}
+
+List  to set  
+['key1', 'value1']
+{'value1', 'key1'}
+
+Tuple to list 
+('key1', 'value1') 
+['key1', 'value1']
+
+List to tuple 
+['key1', 'value1']
+('key1', 'value1')
+'''
