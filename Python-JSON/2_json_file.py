320a949f433ad30fbc4701b8fcd4600050e84716
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-07 23:26:32 -0400

    Create 2_json_file.py

---
+++ Python-JSON/2_json_file.py
@@ -0,0 +1,42 @@
+#Load JSON files to Python Object
+import json
+
+with open('states.json') as f:
+	data = json.load(f)
+
+print(type(data))                            #<class 'dict'>
+print(type(data['states']))                  #<class 'list'>
+print(type(data['states'][0]))               #<class 'dict'>
+print(type(data['states'][0]['area_codes'])) #<class 'list'>
+
+for state in data['states']:
+	print(state)
+	print(state['name'])
+	print(state['abbreviation'])
+	print(state['area_codes'])
+	print()
+
+
+
+
+#Modify Python objects and load back to JSON file
+
+import json
+
+with open('states.json') as f:
+	data = json.load(f)
+
+for state in data['states']:
+	del state['area_codes']
+
+print(data)
+
+with open('new_states.json', 'w') as f:
+	json.dump(data, f, indent=2)
+
+
+
+#This also works instead of with open
+data = json.load(open('states.json'))
+print(data)
+
