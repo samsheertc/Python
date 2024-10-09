07f7d3c04d2af5ce0b34ca48664c21b6e8075c82
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-09 16:35:49 -0400

    Update exceptions.py

--- Exceptions/exceptions.py
+++ Exceptions/exceptions.py
@@ -1,16 +1,20 @@
-
 try:
     f = open('curruptfile.txt')
-    # if f.name == 'currupt_file.txt':
-    #     raise Exception
+    if f.name == 'currupt_file.txt':
+        raise Exception
+    var = bad_var
+except FileNotFoundError as e:
+    print('File Is Missing')
+    print(e)         #System Generated Error
 except IOError as e:
-    print('First!')
+    print('First!')   #Custom Error
+    print(e)         #System Generated Error
 except Exception as e:
-    print('Second')
+    print('Second')  #Custom Error
+    print(e)         #System Generated Error
 else:
     print(f.read())
     f.close()
 finally:
     print("Executing Finally...")
-
 print('End of program')
