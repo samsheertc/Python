e93afe2fd6396e38ebee392e2e1bdfd179b4b43e
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-09 15:56:48 -0400

    Update 3_Enclosing_Scope.py

--- Scope/3_Enclosing_Scope.py
+++ Scope/3_Enclosing_Scope.py
@@ -1,23 +1,25 @@
 #Enclosing Example
 
+#eg-1
 def outer():
     x = 'outer x'
     def inner():
         x = 'inner x'
-        print(x) #inner x [looks for x in local section and it is found]
+        print(x) #inner x [looks for x in local scope and it is found]
     inner()
-    print(x)     #outer x [looks for x local section and it is found]
+    print(x)     #outer x [looks for x local scope and it is found]
 outer()
 
+#eg-2
 def outer():
     x = 'outer x'
     def inner():
-        print(x) #outer x [looks for x in local section but not found. But x is there in local scope of Enclosing function]
+        print(x) #outer x [Looks for x in local scope but not found. But x is there in local scope of Enclosing function]
     inner()
-    print(x)     #outer x [looks for x local section and it is found]
+    print(x)     #outer x [Looks for x local scope and it is found]
 outer()
 
-
+#eg-3
 #Error
 def outer():
     def inner():
@@ -27,17 +29,19 @@ def outer():
     print(x)
 outer()
 
+#eg-4
 #Change the outer function variable
 def outer():
     x = 'outer x'
     def inner():
-        nonlocal x    #inner x [this will allow us work on local variables of enclosing function]
-        x = 'inner x' #inner x [this will affect the x variable of outer function]
+        nonlocal x    #inner x [This will allow us work on local variables of enclosing function]
+        x = 'inner x' #inner x [This will affect the x variable of outer function]
         print(x)
     inner()
     print(x)
 outer()
 
+#eg-5
 #Wrap-up
 x = 'global x'
 def outer():
