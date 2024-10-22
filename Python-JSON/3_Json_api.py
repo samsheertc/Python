83c87a1d418b59ab655c70f7d0d19f7d7a848edc
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-22 17:06:59 -0400

    Update 3_Json_api.py

--- Python-JSON/3_Json_api.py
+++ Python-JSON/3_Json_api.py
@@ -17,7 +17,7 @@ https://www.floatrates.com/json-feeds.html
 import json
 from urllib.request import urlopen
 
-with urlopen("https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json') as response:
+with urlopen("https://finance.yahoo.com/webservice/v1/symbols/allcurrencies/quote?format=json") as response:
     source = response.read()
 
 print(source)                      #Print as a String
@@ -40,4 +40,4 @@ for item in data['list']['resources']:
     price = item['resource']['fields']['price']
     usd_rates[name] = price
 
-print(50* float(usd_rates['USD/EUR']))
+print(50* float(usd_rates['USD/INR']))
