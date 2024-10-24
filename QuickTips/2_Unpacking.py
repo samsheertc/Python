73b8121000cae27714b3b3dd18ef2e1e2c80c31d
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-23 23:35:24 -0400

    Update 2_Unpacking.py

--- QuickTips/2_Unpacking.py
+++ QuickTips/2_Unpacking.py
@@ -1,5 +1,5 @@
-#Unpacking 
-
+#-------------------------------------------------
+#Unpacking
 #Normal
 items = (1, 2)
 print(items)
@@ -18,15 +18,13 @@ ValueError: too many values to unpack (expected 2)
 a, b = (1, 2, 3)
 print(a)
 print(b)
-
-
+#-------------------------------------------------
 #Unpacking
 #ValueError: too many values to unpack (expected 3)
 a, b, c = (1, 2, 3, 4, 5)
 print(a)
 print(b)
 print(c)
-
 
 a, b, *c = (1, 2, 3, 4, 5)
 print(a)
@@ -37,16 +35,15 @@ a, b, *_ = (1, 2, 3, 4, 5)
 print(a)
 print(b)
 
-
 a, b, *c, d = (1, 2, 3, 4, 5, 6,7)
 print(a)
 print(b)
 print(c)
 print(d)
 
-
 a, b, *_, d = (1, 2, 3, 4, 5, 6,7)
 print(a)
 print(b)
 #print(c)
 print(d)
+#-------------------------------------------------
