dacb80335a92865ed2123b336024a5ae1a5c49fd
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-24 11:25:38 -0400

    Update iter-demo_sam1.py

--- Python/Iterators/iter-demo_sam1.py
+++ Python/Iterators/iter-demo_sam1.py
@@ -16,6 +16,7 @@ for num in nums:
 If something is Iterable it needs to have a special method called a dunder method called __iter__()
 
 Iterator is an object with a state so that it remembers where it is during iteration.
+For Iterator it should have __iter__() and __next__() methods by default 
 
 #Check the method avaiable for a List
 nums = [1, 2, 3]
