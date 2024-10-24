ffde8631639fec737e4dd7ec942c3fe791d9b4e7
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-23 23:36:23 -0400

    Update 4_Getpass.py

--- QuickTips/4_Getpass.py
+++ QuickTips/4_Getpass.py
@@ -1,10 +1,13 @@
+#-------------------------------------------------
 #Normal Method
 username = input('Username: ')
 password = input('Password: ')
 print('Logging In....')
 
+#-------------------------------------------------
 #GetPass
 from getpass import getpass
 username = input('Username: ')
 password = getpass('Password: ')
 print('Logging In....')
+#-------------------------------------------------
