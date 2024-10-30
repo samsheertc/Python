11814ca3718484ef4f2e5e3b235e5fc4d9280ba0
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-29 22:08:34 -0400

    Update log_sample.py

--- Logging-Basics/log_sample.py
+++ Logging-Basics/log_sample.py
@@ -6,21 +6,17 @@ def add(x, y):
     """Add Function"""
     return x + y
 
-
 def subtract(x, y):
     """Subtract Function"""
     return x - y
-
 
 def multiply(x, y):
     """Multiply Function"""
     return x * y
 
-
 def divide(x, y):
     """Divide Function"""
     return x / y
-
 
 num_1 = 20
 num_2 = 10
