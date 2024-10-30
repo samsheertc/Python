8f880f0521a883799a9f1c51a42e9eb3123599e2
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-29 22:05:56 -0400

    Update readMe.py

--- Logging-Advanced/readMe.py
+++ Logging-Advanced/readMe.py
@@ -7,7 +7,7 @@ print(logging.WARN)      #30
 print(logging.ERROR)     #40
 print(logging.CRITICAL)  #50
 print(logging.FATAL)     #50
-
+#------------------------------------------------------------------------------------------------------------------------
 import logging
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s:%(name)s:%(levelname)s:%(message)s')
 
@@ -39,22 +39,6 @@ logging.StreamHandler().setLevel
 logging.StreamHandler().set_name
 logging.StreamHandler().setStream
 logging.StreamHandler().formatter
-
-
-logger = logging.getLogger(__name__)
-logger.setLevel(logging.DEBUG)
-
-formatter = logging.Formatter('%(asctime)s:%(name)s:%(message)s')
-
-file_handler = logging.FileHandler('sample.log')
-file_handler.setLevel(logging.ERROR)
-file_handler.setFormatter(formatter)
-
-stream_handler = logging.StreamHandler()
-stream_handler.setFormatter(formatter)
-
-logger.addHandler(file_handler)
-logger.addHandler(stream_handler)
 #------------------------------------------------------------------------------------------------------------------------
 
     
