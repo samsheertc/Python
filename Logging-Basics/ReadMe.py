9c5a639b96e4de9bfc69875a142a7f5fad0b71e8
samsheertc <92280598+samsheertc@users.noreply.github.com>
2024-10-29 22:03:49 -0400

    Create ReadMe.py

---
+++ Logging-Basics/ReadMe.py
@@ -0,0 +1,22 @@
+#---------------------------------------------------------------------------------------------------------------------------------
+# DEBUG: Detailed information, typically of interest only when diagnosing problems.
+# INFO: Confirmation that things are working as expected.
+# WARNING: An indication that something unexpected happened, or indicative of some problem in the near future (e.g. ‘disk space low’). The software is still working as expected.
+# ERROR: Due to a more serious problem, the software has not been able to perform some function.
+# CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
+
+# Default level for logging is set to WARNING. It will capture everything that is a WARNING or above
+# So by default it will log WARNING, ERROR and CRITICAL. It will ignore INFO and DEBUG log statements
+#---------------------------------------------------------------------------------------------------------------------------------
+
+
+#Level values in numbers
+#logging.DEBUG    = 10
+#logging.INFO     = 20
+#logging.WARNING  = 30
+#logging.ERROR    = 40
+#logging.CRITICAL = 50
+
+
+add log record attributes
+https://docs.python.org/3/library/logging.html#logrecord-attributes
